10a0d751cc1899dc4b127dc1d33c1cad5b687ae3
Dhendi Syafa <dhendisyafa@gmail.com>
2023-02-06 13:14:33 +0700

    feat: add onclick function for detail pokemon

--- src/components/pokemonCard/PokemonCard.jsx
+++ src/components/pokemonCard/PokemonCard.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Modal from '../Modal';
 import pokeball from '../../images/icon/pokeball.png'
 
-function PokemonCard({ id, name, image, type, weight, height, stats, statsName }) {
+function PokemonCard({ id, name, image, type, weight, height, stats, statsName, onClick }) {
 
     // const [isShown, setIsShown] = useState(false);
     // const [modalIsOpen, setModalIsOpen] = useState(false);
@@ -16,7 +16,7 @@ function PokemonCard({ id, name, image, type, weight, height, stats, statsName }
     // }
 
     return (
-        <div className="w-full flex items-center justify-between hover:bg-accent rounded border-b-accent border-4 hover:text-white cursor-pointer shadow-md">
+        <div onClick={onClick} className="w-full flex items-center justify-between hover:bg-accent rounded border-b-accent border-4 hover:text-white cursor-pointer shadow-md">
             <div className='flex items-center'>
                 <img src={image} alt={name} className='w-14 h-14 ml-2' />
                 <p className='font-semibold ml-4'>No. {id}</p>
